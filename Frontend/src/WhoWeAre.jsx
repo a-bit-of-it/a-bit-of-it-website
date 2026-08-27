1de58f686be6eb466a2c61acf346b0a5c5fc00e0
a-bit-of-bytes <info@a-bit-of-it.com>
2026-08-27 20:37:09 +0200

    Point to secrets + cleanup bad css

--- Frontend/src/WhoWeAre.jsx
+++ Frontend/src/WhoWeAre.jsx
@@ -1,13 +1,10 @@
 import './WhoWeAre.css'
-import {getWeatherForecast} from "./services/contactService.js";
 
 export default function WhoWeAre ()
 {
     return (
         <div className="who-we-are" id="about">
-            <button onClick={getWeatherForecast}>Get Weather Forecast</button>
-
-            <h1>Who we are</h1>
+            {/*<h1>Who we are</h1>*/}
         </div>
     );
 }
