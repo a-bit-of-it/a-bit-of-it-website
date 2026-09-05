aa564e0c5b8f8170a66369558b444972836d9930
a-bit-of-bytes <info@a-bit-of-it.com>
2026-09-05 14:35:17 +0200

    Fix expanding footer

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
+import './main.css'
 import './i18n'
 import App from './App.jsx'
 import Footer from "./Footer.jsx";
@@ -10,9 +11,13 @@ import Banner from "./components/banner/Banner.jsx";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-        <Banner/>
-        <App />
-        <Footer/>
+        <div className="site">
+            <Banner/>
+            <main className="site-content">
+                <App />
+            </main>
+            <Footer/>
+        </div>
     </BrowserRouter>
   </StrictMode>,
 )
