e88fa70b2c8339204a197f7412ffb32fe395fc6f
a-bit-of-bytes <info@a-bit-of-it.com>
2026-09-05 14:53:49 +0200

    Move date calculation to utilities

--- Frontend/src/pages/purpose/Purpose.jsx
+++ Frontend/src/pages/purpose/Purpose.jsx
@@ -2,28 +2,9 @@ import './Purpose.css';
 import {useTranslation} from "react-i18next";
 import SectionHeading from "../../components/SectionHeading.jsx";
 import GithubLink from "./components/GithubLink.jsx";
-
+import {timeSince} from "../../utilities/timeSince.js";
 
 const MATHIAS_START_DATE = new Date(2018, 8, 1);
-
-function timeSince(startDate) {
-    const now = new Date();
-
-    let years = now.getFullYear() - startDate.getFullYear();
-    let months = now.getMonth() - startDate.getMonth();
-    let days = now.getDate() - startDate.getDate();
-
-    if (days < 0) {
-        months -= 1;
-        days += new Date(now.getFullYear(), now.getMonth(), 0).getDate();
-    }
-    if (months < 0) {
-        years -= 1;
-        months += 12;
-    }
-
-    return { years, months, days };
-}
 
 export default function Purpose() {
     const { t } = useTranslation();
