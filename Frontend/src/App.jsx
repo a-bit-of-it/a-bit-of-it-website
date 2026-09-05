76f968bb0f9ac1e8ce91a574e70ba3395cd5e014
a-bit-of-bytes <info@a-bit-of-it.com>
2026-09-05 22:14:38 +0200

    Privacy policy + consistent CSS

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
 import Purpose from "./pages/purpose/Purpose.jsx";
 import Contact from "./pages/contact/Contact.jsx";
+import PrivacyPolicy from "./pages/privacyPolicy/PrivacyPolicy.jsx";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <Route path="/*" element={<Home/>} />
             <Route path="/purpose" element={<Purpose/>} />
             <Route path="/contact" element={<Contact/>} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy/>} />
         </Routes>
     )
 }
---
+++ Frontend/src/pages/privacyPolicy/PrivacyPolicy.jsx
@@ -0,0 +1,9 @@
+﻿import SectionHeading from "../../components/SectionHeading.jsx";
+
+export default function PrivacyPolicy() {
+    return (
+        <div className="privacy-policy">
+            <SectionHeading header="Privacy Policy" subtitle="When using our contact forms individuals provide their name, email, company affiliation and message content. We use that information to schedule meetings or engage in further conversation. We retain the data only as long as necessary to handle the inquiry and communication."/>
+        </div>
+    );
+}
